e18d74ca555201619763cf0954d2e0c46d2104af
Fabian Buitrago <fabianbuitragov@gmail.com>
2017-07-25 11:19:00 -0500

    Guess the secret number implementation in ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  num: number;
+  message: string = '...';
+  secretNum: number;
+  attempts: number = 10;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit() {
+    this.randomNumber(0, 100);
+  }
+
+  randomNumber(a, b) {
+    this.secretNum = Math.round(
+      Math.random() * (b - a) + parseInt(a)
+    );
+  }
+
+  CheckNumber() {
+    if (this.num) {
+      if (this.secretNum < this.num) {
+        this.message = 'minor';
+        this.attempts -= 1;
+      }
+      else if (this.secretNum > this.num) {
+        this.message = 'major';
+        this.attempts -= 1;
+      }
+      else {
+        this.message = 'equal';
+      }
+    }
+  }
+
+  replay() {
+    this.num = null;
+    this.message = '...';
+    this.randomNumber(0, 100);
+    this.attempts = 10;
+  }
 }
